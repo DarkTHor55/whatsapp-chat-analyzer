4d85b210891838260e20f76cc457e29c7537af40
DarkTHor55 <negir166661@gmail.com>
2025-11-08 10:09:29 +0530

    analysing feature added

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import pandas as pd
 import streamlit as st
+from matplotlib import pyplot as plt
+
 import preprocessor
 import helper
+
 
 
 st.sidebar.title("Whatsapp Chat Analyzer")
@@ -19,7 +23,7 @@ if upload_file is not None:
     selected_user=st.sidebar.selectbox("show analysis wrt",user_list)
 
     if st.sidebar.button("Show Analyze"):
-        num_messages=helper.fetch_stats(selected_user,df)
+        num_messages,words,num_media, y=helper.fetch_stats(selected_user,df)
 
         col1,col2,col3,col4 =st.columns(4)
 
@@ -27,9 +31,80 @@ if upload_file is not None:
             st.write("Total Message")
             st.title(num_messages)
         with col2:
-            st.write("Column 2")
+            st.write("Total words")
+            st.title(len(words))
         with col3:
-            st.write("Column 3")
+            st.write("Total Media")
+            st.title(num_media)
         with col4:
-            st.write("Column 4")
+            st.write("Total Links")
+            st.title(y)
+
+        # most actve user in group
+        if selected_user == "OverAll":
+            st.title("Most Active Users")
+
+            bar_fig, pie_fig, percentage = helper.groupby_stats(df)
+
+            col1, col2 ,col3= st.columns(3)
+
+            with col1:
+                st.subheader("Top 5 Active Users (Bar Chart)")
+                st.pyplot(bar_fig)
+
+            with col2:
+                st.subheader("User Message Share (%) (Pie Chart)")
+                st.pyplot(pie_fig)
+
+            with col3:
+                st.subheader("User Contribution Table")
+                st.dataframe(percentage)
+
+    st.title("Word Cloud")
+    df_wc=helper.create_wordCould(selected_user, df)
+    fig,ax=plt.subplots()
+    ax.imshow(df_wc)
+    st.pyplot(fig)
+
+    st.title("Most Comman words")
+    most_comman_df= helper.most_used_words(selected_user, df)
+    fig,ax=plt.subplots()
+    ax.barh(most_comman_df['Word'], most_comman_df['Count'])
+    plt.xticks(rotation=90)
+
+    st.pyplot(fig)
+
+    st.title("Emoji Analysis")
+    emojis=helper.emoji_helper(selected_user, df)
+
+    col1,col2 =st.columns(2)
+    with col1:
+        st.write("Emoji DataFrame")
+        st.dataframe(emojis)
+    with col2:
+        st.write("Emoji PieChart")
+        if not emojis.empty:
+            top_emojis = emojis.head(10)
+            fig, ax = plt.subplots()
+
+            ax.pie(
+                top_emojis['count'],
+                labels=top_emojis['emoji'],
+                autopct='%1.1f%%',
+                startangle=90,
+                wedgeprops={'edgecolor': 'black'}
+            )
+
+            ax.set_title("Top 10 Emojis Used")
+            st.pyplot(fig)
+        else:
+            st.warning("No emojis found in chat.")
+
+
+
+
+
+
+
+
 
--- helper.py
+++ helper.py
@@ -1,6 +1,97 @@
+from setuptools.command.rotate import rotate
+from urlextract import URLExtract
+import matplotlib.pyplot as plt
+from  collections import Counter
+import numpy as np
+import pandas as pd
+from wordcloud import WordCloud
+import emoji
+
 def fetch_stats(selected_user,df):
-    if selected_user == "OverAll":
-        return df.shape[0]
-    else:
-        return df[df['user']==selected_user].shape[0]
+    if selected_user != "OverAll":
+        df = df[df['user'] == selected_user]
+    nums_message = df.shape[0]
+    words = []
+    num_media=df[df["message"]=="<Media omitted>\n"].shape[0]
+    y=[]
+    urlextract=URLExtract()
+    for message in df['message']:
+        words.extend(message.split())
+        y.extend(urlextract.find_urls(message))
+    return nums_message, words , num_media, len(y)
+
+def groupby_stats(df):
+    x = df['user'].value_counts().head()
+    names = x.index
+    counts = x.values
+    percentage = (
+        (df['user'].value_counts(normalize=True) * 100)
+        .round(2)
+        .reset_index()
+    )
+    percentage.columns = ['user', 'percent']
+
+    fig1, ax1 = plt.subplots()
+    ax1.bar(names, counts, color='skyblue')
+    plt.xticks(rotation='vertical')
+    plt.xlabel("Users")
+    plt.ylabel("Messages")
+    plt.title("Top 5 Most Active Users")
+
+
+    fig2, ax2 = plt.subplots(figsize=(6, 6))
+    ax2.pie(
+        percentage['percent'],
+        labels=percentage['user'],
+        autopct='%1.1f%%',
+        startangle=90,
+        wedgeprops={'edgecolor': 'white'},
+        shadow=True
+    )
+    ax2.set_title("Message Contribution (%) by Users")
+    ax2.axis('equal')
+
+    return fig1, fig2, percentage
+
+def create_wordCould(selected_user,df):
+    if selected_user != "OverAll":
+        df = df[df['user'] == selected_user]
+
+    wc=WordCloud(width=500, height=500,min_font_size=10, background_color='white')
+    df_wc= wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
+
+def most_used_words(selected_user, df):
+    if selected_user != "OverAll":
+        df = df[df['user'] == selected_user]
+
+    df = df[df['user'] != 'group_notification']
+    df = df[df['message'] != '<Media omitted>\n']
+    df = df.dropna(subset=['message'])
+
+    with open("stop_hinglish.txt", 'r', encoding='utf-8') as f:
+        stop_words = f.read().split()
+
+    words = []
+    for message in df['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    return_df = pd.DataFrame(Counter(words).most_common(10), columns=['Word', 'Count'])
+    return return_df
+
+def emoji_helper(selected_user, df):
+    if selected_user != "OverAll":
+        df = df[df['user'] == selected_user]
+    emojis=[]
+    for message in df['message']:
+      for c in message:
+          if emoji.is_emoji(c):
+              emojis.append(c)
+
+    emoji_Counter=Counter(emojis)
+    emoji_df=pd.DataFrame(emoji_Counter.most_common(len(emoji_Counter)), columns=['emoji','count'])
+    return emoji_df
+
     
